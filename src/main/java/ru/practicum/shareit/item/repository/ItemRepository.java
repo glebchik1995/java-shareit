657f2dc9cf8ff868acf4030614459d07ffb6c354
Gleb Verbickiy <gleb.verbickiy@yandex.ru>
2023-07-24 20:20:54 +0300

    Учел и исправил все рекомендации(3 ревью)

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -1,11 +1,9 @@
 package ru.practicum.shareit.item.repository;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
 
