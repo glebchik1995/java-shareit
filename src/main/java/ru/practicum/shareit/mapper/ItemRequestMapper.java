1be9b19f0ea03b2ddc208a97d19b2711fdbbe1ba
Gleb Verbickiy <gleb.verbickiy@yandex.ru>
2023-08-03 19:47:16 +0300

    15 тз

--- src/main/java/ru/practicum/shareit/mapper/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/mapper/ItemRequestMapper.java
@@ -9,6 +9,7 @@ import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
 @UtilityClass
 public class ItemRequestMapper {
     public static ItemRequest toItemRequestModel(ItemRequestShortDto itemRequestShortDto, User user, LocalDateTime now) {
