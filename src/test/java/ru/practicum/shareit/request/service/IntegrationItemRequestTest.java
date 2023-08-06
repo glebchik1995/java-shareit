d7a060e63f4428263c85039b24ccbcb8a173800f
Gleb Verbickiy <gleb.verbickiy@yandex.ru>
2023-08-06 20:58:09 +0300

    2 ревью. Исправил ошибки

--- src/test/java/ru/practicum/shareit/request/service/IntegrationItemRequestTest.java
+++ src/test/java/ru/practicum/shareit/request/service/IntegrationItemRequestTest.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exceptions.DataNotFoundException;
 import ru.practicum.shareit.item.service.ItemServiceImpl;
